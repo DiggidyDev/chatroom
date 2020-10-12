caa3f8e57e5a55b76ca775b8bf0fe1c741fe91de
DiggidyDev <valentinewilson@gmail.com>
2020-10-12 20:39:55 +0100

    Time elapsed when executing functions

--- utils/debug.py
+++ utils/debug.py
@@ -1,10 +1,22 @@
+import time
+
+
 def debug(verbose: bool = False):
     def inner(function):
         def wrapper(*args, **kwargs):
             if verbose:
                 log(f"Entering {function.__name__}")
+
+                # For calculating the time elapsed in the function
+                startTime = time.perf_counter()
+
+                # Run decorated function, grabbing the returned value
                 _return = function(*args, **kwargs)
-                log(f"Returning {_return} - {type(_return)}")
+
+                endTime = time.perf_counter()
+
+                log(f"Returning {_return} - {type(_return)}", function)
+                log(f"Elapsed time: {round(endTime*1000 - startTime*1000, 5)}ms", function)
                 log(f"Exiting {function.__name__}")
 
         return wrapper
@@ -12,5 +24,8 @@ def debug(verbose: bool = False):
     return inner
 
 
-def log(string):
-    print(f"[DEBUG] {string}")
+def log(string, function: callable = None):
+    if not function:
+        print(f"[DEBUG] {string}")
+    else:
+        print(f"[DEBUG] [{function.__name__} @ {function.__module__}] {string}")
