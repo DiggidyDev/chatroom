c2e6b5ce370b734b65205a7072f2efe2f9e5dae2
DiggidyDev <valentinewilson@gmail.com>
2020-10-12 20:39:06 +0100

    Changed to accept sender's nickname

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import selectors
 import socket
+import socketserver
 import types
 
 from utils.debug import debug
@@ -12,33 +13,40 @@ HOST = "127.0.0.1"
 class Server:
 
     def __init__(self, host: str, port: int):
+        self.conn = (host, port)
+        self.clients = []
         self.host = host
         self.port = port
-        self.conn = (host, port)
 
-    @debug(verbosity=2)
-    def accept_wrapper(self, sock, sel: selectors.DefaultSelector):
+    @debug(verbose=True)
+    def accept_wrapper(self, sock, sel: selectors.DefaultSelector) -> None:
         # Accept the incoming connection
         conn, addr = sock.accept()
         print(f"Connection successfully made from {':'.join(str(i) for i in addr)}")
         conn.setblocking(False)
+
+        # Give data some attributes
         data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
+        # Register the connection to allow for reading and writing
         sel.register(conn, events=events, data=data)
 
-    def conn_multi(self):
+    @debug(verbose=True)
+    def conn_multi(self) -> None:
         sel = selectors.DefaultSelector()
 
         # Create and bind the socket
         lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        lsock.bind((HOST, PORT))
+        lsock.bind(self.conn)
 
-        # Listen, don't use blocking calls
+        # Listen for connections, don't use blocking calls
         lsock.listen()
         lsock.setblocking(False)
 
         # Socket registration
         sel.register(lsock, selectors.EVENT_READ, data=None)
+        print(f"Hosting on PORT {self.port}")
 
         while True:
             events = sel.select(timeout=None)
@@ -48,7 +56,7 @@ class Server:
                 else:
                     self.service_connection(k, mask, sel)
 
-    def conn_single(self):
+    def conn_single(self) -> None:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
 
@@ -65,7 +73,8 @@ class Server:
 
                     conn.sendall(f"Hello {addr[0]}!".encode("utf-8"))
 
-    def service_connection(self, key, mask, sel: selectors.DefaultSelector):
+    @staticmethod
+    def service_connection(key, mask, sel: selectors.DefaultSelector):
         sock = key.fileobj
         data = key.data
 
@@ -73,7 +82,6 @@ class Server:
         if mask & selectors.EVENT_READ:
             recv_data = sock.recv(1024)
             if recv_data:
-                print(dir(data))
                 print(f"Received {repr(recv_data)} from {data.addr}")
                 data.outb += recv_data
 
