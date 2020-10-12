34ced37f01a5f90b03087748517f2b4a8caebde8
DiggidyDev <valentinewilson@gmail.com>
2020-10-12 20:40:34 +0100

    Send username from user info

--- client/client.py
+++ client/client.py
@@ -1,12 +1,22 @@
 import socket
 
+from user import User
+from utils.debug import debug
+
 PORT = 65501
 HOST = "127.0.0.1"
 
+u = User("Nickname")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    a = s.sendall(b"Hello :)")
-    data = s.recv(1024)
 
-    print(f"Received: {data.decode('utf-8')}")
+@debug(verbose=True)
+def connect():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(f"Hello from {u}".encode("utf-8"))
+        data = s.recv(1024)
+
+        print(f"Received: {data.decode('utf-8')}")
+
+
+connect()
