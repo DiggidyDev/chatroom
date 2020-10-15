815bd7a7d9cbb0462389d57f36095ec5ba8e7416
DiggidyDev <valentinewilson@gmail.com>
2020-10-15 18:35:34 +0100

    GDPR Pop-out GUI

---
+++ ui/gdpr.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets
+
+
+class GDPRWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setupUI()
+
+    def setupUI(self):
+        pass
