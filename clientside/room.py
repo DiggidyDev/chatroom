b6a92394879c62d579847f648b35e6db02f69a46
DiggidyDev <valentinewilson@gmail.com>
2020-11-13 12:14:22 +0000

    Baseclasses for chatrooms

---
+++ clientside/room.py
@@ -0,0 +1,15 @@
+class Room:
+
+    def __init__(self):
+        pass
+
+
+class Chatroom(Room):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Gameroom(Chatroom):
+    def __init__(self):
+        super().__init__()
