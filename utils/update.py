49b6f1cb1950a1bc09f8ebbb8c39b91ffaf22e9c
DiggidyDev <valentinewilson@gmail.com>
2020-12-05 12:16:08 +0000

    Re-purposed for auto-updating

--- utils/update.py
+++ utils/update.py
@@ -1,39 +1,83 @@
-import pickle
-from datetime import datetime
+# Set the current application's download type
+# based on the modules available to be imported
+try:
+    from utils.versionbin import get_version, set_version
+    DL_TYPE = "exe"
+except ImportError:
+    from utils.versionzip import get_version, set_version
+    DL_TYPE = "zip"
 
-from PyQt5 import QtWidgets
+
+def check_for_updates() -> bool:
+    """
+    Will return True if any updates are found.
+
+    Using the queried version from the website, this checks
+    whether the current client's version is up-to-date. If
+    it isn't, it will [PERFORM UPDATE].
+    :return:
+    """
+
+    from utils.versionbin import get_version
+
+    if get_live_version() != get_version():
+        return True
+    return False
 
 
-def update_msg_list(widget: QtWidgets.QMainWindow, received: dict):
-    item = QtWidgets.QListWidgetItem()
-    if isinstance(received, bytes):
-        received = pickle.loads(received)
-    sender = str(received["user"])
+def get_live_version() -> str:
+    """
+    Fetches the current version of the client being hosted
+    on the website. This will be used to check for new versions
+    and auto-updating the application.
 
-    # Display `You` for messages you, the user, have sent
-    # as to distinguish messages in an easier fashion
-    if hasattr(widget, "user"):
-        if received["user"].get_uuid() == widget.user.get_uuid():
-            sender = "You"
+    :return:
+    """
+    import json  # For converting the requested string into JSON
+    import urllib.request  # Querying for the current live version
 
-    currentTime = datetime.now()
+    live_version = json.loads(urllib.request.urlopen("https://diggydev.co.uk/chatroom/version.json").read())["live_bin"]
 
-    if received['system-message']:
-        if received['content'] == "Initial connection.":
-            item.setText(f"{sender} joined.")
-        elif received['content'] == "Leaving":
-            item.setText(f"{sender} left.")
-        else:
-            item.setText(received["content"])
-    elif not received['system-message']:
-        item.setText(f"{sender}: {received['content'].replace('fuck', '****')}")
+    return live_version
 
-    item.setToolTip(fr"""Timestamp: {currentTime.strftime("%Y/%m/%d - %H:%M:%S")}
-UUID: {received['user'].get_uuid() if received['user'] != 'Server' else None}""".strip())
-    try:
-        widget.msgList.addItem(item)
-    except RuntimeError:
-        return
 
-    widget.msgList.scrollToBottom()
+def start_download() -> bool:
+    if DL_TYPE == "exe":
+        try:
+            import os
+            import requests
 
+            CHNK_SIZE = 2**20
+
+            if not os.path.isfile("updater.exe"):
+                updater = requests.get(f"https://diggydev.co.uk/chatroom/updater.exe", stream=True)
+
+                with open("updater.exe", "wb") as up_exe:
+                    for chnk in updater.iter_content(chunk_size=CHNK_SIZE):
+                        up_exe.write(chnk)
+
+                updater.close()
+
+            new_client = requests.get(f"https://diggydev.co.uk/chatroom/client.{DL_TYPE}", stream=True)
+            print(new_client)
+            with open("client-new.exe", "wb") as exe:
+                for chnk in new_client.iter_content(chunk_size=CHNK_SIZE):  # 1 MiB
+                    exe.write(chnk)
+
+            new_client.close()
+        except Exception as e:
+            print(e)
+        finally:
+            import os
+            import sys
+
+            set_version(get_live_version())
+
+            os.execl("updater.exe", *([sys.executable] + sys.argv))
+    if DL_TYPE == "zip":
+        from zipfile import ZipFile
+
+        with ZipFile("client.zip", "r") as _zip:
+            _zip.extractall()
+
+    return True
