ea362faeef07087bac34d4418c811b9c33a4abe9
DiggidyDev <valentinewilson@gmail.com>
2020-12-09 23:29:24 +0000

    Added password hasher and email validation

--- utils/fmt.py
+++ utils/fmt.py
@@ -1,4 +1,6 @@
+import hashlib
 import pickle
+import re
 from datetime import datetime
 
 from PyQt5 import QtWidgets
@@ -18,35 +20,47 @@ def content(**kwargs):
 
     return struct
 
+
+def hash_pw(password: str, uuid: str) -> str:
+    return hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), uuid.encode("utf-8"), 200000).hex()
+
+
 def update_msg_list(widget: QtWidgets.QMainWindow, received: dict):
-    item = QtWidgets.QListWidgetItem()
-    if isinstance(received, bytes):
-        received = pickle.loads(received)
-    sender = str(received["user"])
+    if "user" in received.keys():
+        item = QtWidgets.QListWidgetItem()
+        if isinstance(received, bytes):
+            received = pickle.loads(received)
+        sender = str(received["user"])
 
-    # Display `You` for messages you, the user, have sent
-    # as to distinguish messages in an easier fashion
-    if hasattr(widget, "user"):
-        if received["user"].get_uuid() == widget.user.get_uuid():
-            sender = "You"
+        # Display `You` for messages you, the user, have sent
+        # as to distinguish messages in an easier fashion
+        if hasattr(widget, "user"):
+            if received["user"].get_uuid() == widget.user.get_uuid():
+                sender = "You"
 
-    currentTime = datetime.now()
+        currentTime = datetime.now()
 
-    if received['system-message']:
-        if received['content'] == "Initial connection.":
-            item.setText(f"{sender} joined.")
-        elif received['content'] == "Leaving":
-            item.setText(f"{sender} left.")
-        else:
-            item.setText(received["content"])
-    elif not received['system-message']:
-        item.setText(f"{sender}: {received['content'].replace('fuck', '****')}")
+        if received['system-message']:
+            if received['content'] == "Initial connection.":
+                item.setText(f"{sender} joined.")
+            elif received['content'] == "Leaving":
+                item.setText(f"{sender} left.")
+            else:
+                item.setText(received["content"])
+        elif not received['system-message']:
+            # EXPLICIT FILTER
+            item.setText(f"{sender}: {received['content'].replace('fuck', '****')}")
 
-    item.setToolTip(fr"""Timestamp: {currentTime.strftime("%Y/%m/%d - %H:%M:%S")}
-UUID: {received['user'].get_uuid() if received['user'] != 'Server' else None}""".strip())
-    try:
-        widget.msgList.addItem(item)
-    except RuntimeError:
-        return
+        item.setToolTip(fr"""Timestamp: {currentTime.strftime("%Y/%m/%d - %H:%M:%S")}
+    UUID: {received['user'].get_uuid() if received['user'] != 'Server' else None}""".strip())
+        try:
+            widget.msgList.addItem(item)
+        except RuntimeError:
+            return
 
-    widget.msgList.scrollToBottom()
+        widget.msgList.scrollToBottom()
+
+
+def is_valid_email(email: str) -> re.Match:
+    email_pattern = r"^(\w+[\+\.\w-]*@)([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|d+)$"
+    return re.search(email_pattern, email)
