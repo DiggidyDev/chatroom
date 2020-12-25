d0a943f0fedc43adebfc7d0cf94ece17c8684992
DiggidyDev <valentinewilson@gmail.com>
2020-12-25 17:42:03 +0000

    For future use

--- utils/versionzip.py
+++ utils/versionzip.py
@@ -1,8 +1,11 @@
 def get_version() -> str:
     f = open("version.txt", "r+")
-    return f.read()
+    contents = f.read()
+    f.close()
+    return contents
 
 
 def set_version(version: str):
     f = open("version.txt", "w+")
     f.write(version)
+    f.close()
