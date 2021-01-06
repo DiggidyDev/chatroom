b679ed18713518c7bab7e1ae9d6eb188d172be91
Diggy <valentinewilson@gmail.com>
2021-01-06 00:13:58 +0000

    Font now changes for window pop-ups (ref: cool mode)

--- clientside/actionslots.py
+++ clientside/actionslots.py
@@ -19,20 +19,22 @@ class ViewSlots:
 class HelpSlots:
 
     @staticmethod
-    def about_chatroom() -> None:
+    def about_chatroom(widget: clientside.client.Client) -> None:
         chatroom_version = get_version()
 
         info_window = CustomDialog(window_title="About Chatroom",
                                    message=f"**Current Version:** {chatroom_version}\n\n"
                                            f"**Developer:** Valentine Wilson\n\n"
-                                           f"*[Source code](https://github.com/DiggidyDev/chatroom)*")
+                                           f"*[Source code](https://github.com/DiggidyDev/chatroom)*",
+                                   font=widget.current_font)
         info_window.exec_()
 
     @staticmethod
-    def faq() -> None:
+    def faq(widget: clientside.client.Client) -> None:
         faq_window = CustomDialog(window_title="FAQ",
                                   message="Q: something\n\n"
-                                          "A: another thing")
+                                          "A: another thing",
+                                  font=widget.current_font)
         faq_window.exec_()
 
     @staticmethod
