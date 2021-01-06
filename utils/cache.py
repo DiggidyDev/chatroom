24e12970d17cd48c7b68ed37f98a4213125800e0
Diggy <valentinewilson@gmail.com>
2021-01-06 00:19:24 +0000

    Exception raising, fetching and updating cache

--- utils/cache.py
+++ utils/cache.py
@@ -1,11 +1,13 @@
 import timeit
-from typing import List, Dict, Union
-from uuid import UUID, uuid4
+from typing import List, Dict, Union, Iterable
+from uuid import UUID
 
-from bases import _BaseObj
+from bases import BaseObj
+from room import Room
+from user import User
 
 
-class _CacheDict(Dict[str, _BaseObj], dict):
+class _CacheDict(Dict[str, BaseObj], dict):
     """
     My own implementation of a dict, used for fetching
     objects when populated, given a specific key.
@@ -27,13 +29,32 @@ class Cache:
         self.__cache_list: List[str] = []
         self.__bottom = 0
 
+    def __delitem__(self, key):
+        print("deleting")
+        self.__cache_list.remove(key)
+        del self.__cache_dict[key]
+
     def __str__(self):
         return ", ".join(self.__cache_list)
 
     def __len__(self):
         return len(self.__cache_list)
 
-    def __uncache_from_index(self, index: int) -> _BaseObj:
+    def __do_cache(self, obj: BaseObj, index: int):
+        try:
+            self.get(obj)
+        except KeyError:
+            if self.is_full():
+                self.__uncache_from_index(index)
+
+            uuid = str(obj.uuid)
+            self.__cache_list.insert(index, uuid)
+
+            self.__cache_dict[uuid] = obj
+        else:
+            print("Value already exists.")
+
+    def __uncache_from_index(self, index: int) -> BaseObj:
         removed = self.__cache_list.pop(-(index+1 & 1))
         return self.__cache_dict.pop(removed)
 
@@ -41,9 +62,14 @@ class Cache:
     def __top(self) -> int:
         return self.__len__()
 
-    def cache_to(self, side: str, obj: _BaseObj):
+    def cache_to(self, side: str, obj: Union[BaseObj, Iterable[BaseObj]]):
         """
-        Bottom of cache has an index of 0
+        Bottom of cache has an index of 0.
+
+        This only really matters when coming to large
+        amounts of messages being cached clientside
+        when scrolling up or down; "top" and "bottom"
+        respectively.
 
         :param side:
         :param obj:
@@ -56,25 +82,66 @@ class Cache:
         elif side == "top":
             index -= 1
 
-        if self.is_full():
-            self.__uncache_from_index(index)
+        if isinstance(obj, BaseObj):
+            self.__do_cache(obj, index)
+        elif isinstance(obj, Iterable):
+            for iter_obj in obj:
+                self.__do_cache(iter_obj, index)
 
-        uuid = str(obj.uuid)
-        self.__cache_list.insert(index, uuid)
+    # TODO: Add get by certain attr?
+    def get(self, obj: ...) -> Union[None, BaseObj, Room, User]:
+        """
+        Returns
 
-        self.__cache_dict[uuid] = obj
-
-    def get(self, obj: ...) -> Union[None, _BaseObj]:
-        """"""
+        :param obj:
+        :return:
+        """
+        if obj is None:
+            raise TypeError("No obj given")
         try:
-            if isinstance(obj, _BaseObj):
+            if isinstance(obj, BaseObj):
                 return self.__cache_dict[str(obj.uuid)]
             elif isinstance(obj, str):
                 return self.__cache_dict[obj]
             elif isinstance(obj, UUID):
                 return self.__cache_dict[str(obj)]
         except KeyError:
-            raise
+            raise KeyError(f"{obj!r} not found in cache")
 
     def is_full(self):
         return self.__top == self.__cache_dict.max_size
+
+    def update(self, obj: BaseObj):
+        if not isinstance(obj, BaseObj):
+            raise TypeError(f"{obj!r} invalid type for cache")
+        try:
+            cached_obj = self.get(obj)
+            self.__cache_dict[str(cached_obj.uuid)] = obj
+        except KeyError:
+            raise
+
+
+def main():
+
+    from uuid import uuid1
+
+    class TestObj(BaseObj):
+
+        @property
+        def uuid(self) -> UUID:
+            return uuid1()
+
+    times = {}
+
+    for i in range(10):
+        mycache = Cache(max_size=2**15)
+        start = timeit.default_timer()
+        for _ in range(2**16):
+            t = TestObj()
+            mycache.cache_to("bottom", t)
+
+        times[i] = timeit.default_timer()-start
+
+    FORMATTED = f""" AVERAGE : {sum(times.values())/len(times)}"""
+
+    print(FORMATTED)
