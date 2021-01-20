acee6f50d5de422dc139da49c1bd13378dc39275
Diggy <valentinewilson@gmail.com>
2021-01-20 23:48:31 +0000

    Recent message fetching from rooms

--- serverside/query.py
+++ serverside/query.py
@@ -34,10 +34,20 @@ class MessageQuery(BaseQuery):
                                        message.system_message))
         self.conn.commit()
 
+    def fetch_recent_messages(self, *, amount=100, room: Room = None):
+        if room is None:
+            room = self.room_query.main_room
+
+        self.cursor.execute(f"""SELECT message_uuid FROM messages WHERE room = ? ORDER BY _id DESC LIMIT {amount}""",
+                            (str(room.uuid),))
+        r = self.cursor.fetchall()
+        msgs = [self.fetch_message_by_uuid(u[0]) for u in r]
+
+        return msgs
+
     def fetch_message_by_uuid(self, uuid: str) -> Message:
         try:
             message = self.cache.get(uuid)
-            print("GOT FROM CACHE")
         except (KeyError, TypeError) as e:
             self.cursor.execute(f"""SELECT {", ".join(Message.TABLE_COLUMNS)} FROM messages WHERE message_uuid = ?""", (uuid,))
             record = self.cursor.fetchone()
@@ -49,7 +59,6 @@ class MessageQuery(BaseQuery):
             obj_record[4] = self.user_query.fetch_user_by("uuid", obj_record[4])
 
             obj_record = (*obj_record,)
-            print(obj_record, "AHWAH")
             message = Message.data_format_from_tuple(obj_record, return_instance=True)
 
             if isinstance(message, Message):
@@ -71,18 +80,18 @@ class RoomQuery(BaseQuery):
         super().__init__(self.__cache, self.__conn, self.__cursor)
         self.create_table_if_not_exists("rooms")
 
-        self.main_room: Room = self.__create_main_room_if_not_exists(Room("main"))
+        self.main_room: Room = self.create_room_if_not_exists(Room("main"))
 
-    def __create_main_room_if_not_exists(self, room: Room) -> Room:
-        main_room = self.fetch_room_by("name", "main")
-        if main_room is None:
+    def create_room_if_not_exists(self, room_to_find: Room) -> Room:
+        room = self.fetch_room_by("name", room_to_find.name)
+        if room is None:
             self.cursor.execute("""INSERT INTO rooms (name, uuid)
             SELECT ?, ? WHERE NOT EXISTS (SELECT 1 FROM rooms WHERE name = "main")""",
-                                (room.name, str(room.uuid)))
+                                (room_to_find.name, str(room_to_find.uuid)))
             self.conn.commit()
-            main_room = room
+            room = room_to_find
 
-        return main_room
+        return room
 
     def fetch_room_by(self, column: str, data: ...) -> Union[None, Room]:
         """
@@ -130,7 +139,7 @@ class UserQuery(BaseQuery):
         self.cache.cache_to("bottom", user)
 
         if not user.is_anonymous():
-            email = user.get_email()
+            email = user.email
             pw_hash = hash_pw(password, uuid)
             self.cursor.execute("""INSERT INTO users (uuid, name, pwhash, email, rooms)
                                 VALUES (?, ?, ?, ?, ?)""", (uuid, name, pw_hash, email,
@@ -184,20 +193,15 @@ class UserQuery(BaseQuery):
 
             return True if record else False
 
-        return self.fetch_pw_hash_by("email", email) if return_pw_hash else self.fetch_user_data_by("email", email)
-
     def fetch_all_rooms_for(self, user: User) -> Iterable[Room]:
         if user.is_anonymous():
             return [self.room_query.main_room]
 
         try:
             cached_user = self.cache.get(user)
-            print(user)
-            print(f"{user!r}")
-            print(vars(cached_user))
 
             return cached_user.rooms
-        except KeyError as e:
+        except (KeyError, AttributeError) as e:
             print(e)
             self.cursor.execute("""SELECT rooms FROM users WHERE uuid = ?""", (str(user.uuid),))
             record = self.cursor.fetchone()[0]
@@ -206,28 +210,29 @@ class UserQuery(BaseQuery):
             rooms = [self.room_query.fetch_room_by("uuid", uuid) for uuid in room_uuids]
             user.rooms = rooms
 
-            self.cache.cache_to("bottom", user)
+            if isinstance(e, KeyError):
+                self.cache.update(user)
+            else:
+                self.cache.cache_to("bottom", user)
 
             return rooms
 
-    def fetch_user_data_by(self, column: str, data: ...) -> Tuple[int, Tuple[Union[str, None], ...]]:
-        self.cursor.execute(f"""SELECT status, uuid, name, friends, blockedusers, nickname, friendrequests, email
-                            FROM users WHERE {column} = ?""", (data,))
-
-        user_data = self.cursor.fetchone()
-
-        return user_data
-
     def fetch_user_by(self, column: str, data: ...):
+        user = None
         try:
             user = self.cache.get(data if column == "uuid" else None)
-        except (KeyError, TypeError) as e:
-            user_data = self.fetch_user_data_by(column, data)
-            user = User.from_existing_data(data=user_data)
-            user.rooms = self.fetch_all_rooms_for(user)
 
-            if isinstance(user, User):
-                self.cache.cache_to("bottom", user)
+        except (KeyError, TypeError) as e:
+            self.cursor.execute(f"""SELECT status, uuid, name, friends, blockedusers, nickname, friendrequests, email, rooms
+                                FROM users WHERE {column} = ?""", (data,))
+
+            user_data = self.cursor.fetchone()
+            if user_data is not None:
+                user = User.from_existing_data(data=user_data)
+                user.rooms = self.fetch_all_rooms_for(user)
+
+                if isinstance(user, User):
+                    self.cache.cache_to("bottom", user)
 
         return user
 
@@ -249,27 +254,4 @@ class UserQuery(BaseQuery):
         self.cursor.execute("""UPDATE users SET status = ? WHERE uuid = ?""", (user.status, str(user.uuid)))
 
         self.conn.commit()
-
-
-if __name__ == "__main__":
-    rm = Cache()
-    ms = Cache()
-    ms_q = MessageQuery(cache=ms)
-    rm_q = RoomQuery(cache=rm)
-    us_q = UserQuery(cache=Cache(max_size=2**10))
-    ms_q.room_query, ms_q.user_query = rm_q, us_q
-    rm_q.msg_query, rm_q.user_query = ms_q, us_q
-    us_q.msg_query, us_q.msg_query = ms_q, rm_q
-
-    msg = Message(content="HELLO WORLD", system_message=True, room=rm_q.fetch_room_by("name", "main"))
-    ms_q.add_message(msg)
-
-    print("FIRST FETCH")
-    m = ms_q.fetch_message_by_uuid("1d7548ec-50b0-f8be-5097-5d22f16f84ca")
-
-    print("FETCHING AGAIN")
-    m = ms_q.fetch_message_by_uuid("1d7548ec-50b0-f8be-5097-5d22f16f84ca")
-
-    print("CLEAN CACHE FETCH:")
-    m = ms_q.clear_cache_for(ms_q.fetch_message_by_uuid, "1d7548ec-50b0-f8be-5097-5d22f16f84ca")
 
