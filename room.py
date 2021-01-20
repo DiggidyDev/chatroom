29d0d7b3a51bf1f8db87a8d184c726be348edcd5
Diggy <valentinewilson@gmail.com>
2021-01-20 23:58:31 +0000

    Added __repr__, recent message attr

--- room.py
+++ room.py
@@ -13,14 +13,22 @@ class Room(BaseObj):
                  *,
                  invitedusers=None,
                  password=None,
-                 uuid=UUID(bytes=token_bytes(16))):
+                 uuid=None,
+                 last_message=None):
+
+        if invitedusers is None:
+            invitedusers = []
 
         self._invites = invitedusers
+        self._last_message = last_message
         self._name = name
         self._password = password
         self._uuid = uuid if uuid else UUID(bytes=token_bytes(16))
 
         super().__init__()
+
+    def __repr__(self):
+        return f"<type={self.__class__.__qualname__} invites={self.invites!r} name={self.name!r} has_password={self._password is not None}>"
 
     def __str__(self):
         return str(self.uuid)
@@ -36,6 +44,14 @@ class Room(BaseObj):
     @name.setter
     def name(self, value):
         self._name = value
+
+    @property
+    def most_recent_message(self):
+        return self._last_message
+
+    @most_recent_message.setter
+    def most_recent_message(self, value):
+        self._last_message = value
 
     @property
     def uuid(self):
